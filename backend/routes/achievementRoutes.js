6d277025256c132ecb1688170de31d9c008ceaa5
isabell-ah <wanzalaisabella@gmail.com>
2025-05-28 17:22:58 +0300

    removed

--- backend/routes/achievementRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAchievements } = require('../controllers/achievementController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
-
-// Get achievements (for current user or specified child)
-router.get('/', authenticateToken, getAchievements);
-
-module.exports = router;
